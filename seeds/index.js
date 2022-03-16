3b90f5de98b653e0ee05ee277fd828afdde6375a
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-03-15 22:07:52 -0700

    New file, Bootstrap page formatting and campground reseeding with images
    campground.js
    +image and price
    
    index.js
    +const variable price set to random number up to 20 + 10
    +image src set to random collection from Unsplash
    +description hard-coded with lorem
    
    show.ejs
    +image and description ejs
    
    boilerplate.ejs
    +Bootstrap body styles to push footer down
    +Ejs include footer
    
    New file:
    footer.ejs
    +Bootstrap footer with brand text

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251/640x426",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nobis provident possimus reiciendis labore? Dolor veritatis mollitia voluptatibus numquam impedit, recusandae minima saepe maxime delectus iusto itaque doloremque similique suscipit? Repellat officia, molestiae deserunt ducimus pariatur iste quia porro saepe perferendis. Corrupti dicta veniam sed molestias dignissimos provident exercitationem autem maxime quibusdam recusandae voluptatum ad, nulla minima molestiae labore consequuntur? Earum, quos, hic dolor eos eum laudantium quasi est perferendis quidem asperiores odit. At quidem, eius, nihil cumque odit magnam ullam, iste consequuntur tempore debitis soluta sint tempora numquam necessitatibus?",
+      price,
     });
     await camp.save();
   }
