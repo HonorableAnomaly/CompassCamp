c72d92291ec629653d4056931a51f7e6f6e14250
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-03-28 19:19:00 -0700

    Client-side form validations and error handler
    new.ejs and edit.ejs
    +Novalidate added to form to allow Bootstrap to handle validation over the browser
    +Required and valid-feedback for all input fields
    
    boilerplate.ejs
    +Bootstrap form validation script
    
    app.js
    +Primitive error handler

--- app.js
+++ app.js
@@ -75,6 +75,10 @@ app.delete("/campgrounds/:id", async (req, res) => {
 //   res.send(camp);
 // });
 
+app.use((err, req, res, next) => {
+  res.send("Oh boy, something went wrong!");
+});
+
 app.listen(3000, () => {
   console.log("Serving on port 3000...");
 });
