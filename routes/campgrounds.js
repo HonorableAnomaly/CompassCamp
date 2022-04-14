dbed8995723f38dbbbc3a1e8d2febe4ec7a02058
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-04-13 20:21:29 -0700

    Campground and review rerouting, new dir/file
    New dir:
    +public
    
    app.js
    +App.use for campgrounds and reviews routes
    +Commented out relocated routes
    
    routes/campgrounds.js
    +Copied over all campgrounds routes from app.js and changed 'app' routing prefix to 'router'
    +Copied over requires for Joi campgroundSchema, catchAsync, ExpressError and Campground model
    +Copied over validateCampground function
    +Object argument 'mergeParams: true' added to router
    +module.exported router
    
    New file:
    router/reviews.js
    ++Copied over all campgrounds routes from app.js and changed 'app' routing prefix to 'router'
    +Copied over requires for express, router, Joi reviewSchema, catchAsync, ExpressError and Campground/Review models
    +Copied over validateReview function
    +Object argument 'mergeParams: true' added to router
    +module.exported router

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,2 +1,75 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router({ mergeParams: true });
+const { campgroundSchema } = require("../schemas.js");
+const catchAsync = require("../utilities/catchAsync");
+const ExpressError = require("../utilities/ExpressError");
+const Campground = require("../models/campground");
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, msg);
+  } else {
+    next();
+  }
+};
+
+router.get(
+  "/",
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
+
+router.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+router.post(
+  "/",
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+router.get(
+  "/:id",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id).populate("reviews");
+    res.render("campgrounds/show", { campground });
+  })
+);
+
+router.get(
+  "/:id/edit",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campground });
+  })
+);
+
+router.put(
+  "/:id",
+  validateCampground,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { new: true });
+    res.render("campgrounds/show", { campground });
+  })
+);
+
+router.delete(
+  "/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
+
+module.exports = router;
