ec078bca43849840b93458ad4d8f3f32b86c71e2
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-04-13 20:57:37 -0700

    Specified static pathing, new dir/file
    app.js
    +Comments added
    +Specified static pathing using 'path.join'
    - Relocated routes removed
    
    New dir/file:
    /public/stylesheets
    /public/javascripts/validateForms
    +Copied over validation script from 'boilerplate.ejs'
    
    boilerplate.ejs
    - Relocated validation script to /public/javascripts/validateForms
    +Script call for /javascripts/validateForms.js

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+// Moved requires to other routers
 // const { campgroundSchema, reviewSchema } = require("./schemas.js");
 // const catchAsync = require("./utilities/catchAsync");
 const ExpressError = require("./utilities/ExpressError");
 const methodOverride = require("method-override");
+// Moved requires to other routers
 // const Campground = require("./models/campground");
 // const Review = require("./models/review");
 
@@ -17,6 +19,8 @@ mongoose.connect("mongodb://localhost:27017/compass-camp"),
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
+    // Used to be necessary to get rid of a depracation warning
+    // useFindAndModify: false
   };
 
 const db = mongoose.connection;
@@ -33,27 +37,8 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-
-// const validateCampground = (req, res, next) => {
-//   const { error } = campgroundSchema.validate(req.body);
-//   if (error) {
-//     // Maps the elements of error.details into a single string message, joined by commas if there is more than one error
-//     const msg = error.details.map((el) => el.message).join(",");
-//     throw new ExpressError(400, msg);
-//   } else {
-//     next();
-//   }
-// };
-
-// const validateReview = (req, res, next) => {
-//   const { error } = reviewSchema.validate(req.body);
-//   if (error) {
-//     const msg = error.details.map((el) => el.message).join(",");
-//     throw new ExpressError(400, msg);
-//   } else {
-//     next();
-//   }
-// };
+// Used to tell express to serve a dir
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
@@ -61,88 +46,6 @@ app.use("/campgrounds/:id/reviews", reviews);
 app.get("/", (req, res) => {
   res.render("home");
 });
-
-// app.get(
-//   "/campgrounds",
-//   catchAsync(async (req, res) => {
-//     const campgrounds = await Campground.find({});
-//     res.render("campgrounds/index", { campgrounds });
-//   })
-// );
-
-// app.get("/campgrounds/new", (req, res) => {
-//   res.render("campgrounds/new");
-// });
-
-// app.post(
-//   "/campgrounds",
-//   validateCampground,
-//   catchAsync(async (req, res, next) => {
-//     // Original error handling for req.body.campground before Joi schema implementation
-//     // if (!req.body.campground) throw new ExpressError(400, "Invalid Campground Data");
-//     const campground = new Campground(req.body.campground);
-//     await campground.save();
-//     res.redirect(`/campgrounds/${campground._id}`);
-//   })
-// );
-
-// app.get(
-//   "/campgrounds/:id",
-//   catchAsync(async (req, res) => {
-//     const campground = await Campground.findById(req.params.id).populate("reviews");
-//     res.render("campgrounds/show", { campground });
-//   })
-// );
-
-// app.get(
-//   "/campgrounds/:id/edit",
-//   catchAsync(async (req, res) => {
-//     const campground = await Campground.findById(req.params.id);
-//     res.render("campgrounds/edit", { campground });
-//   })
-// );
-
-// app.put(
-//   "/campgrounds/:id",
-//   validateCampground,
-//   catchAsync(async (req, res) => {
-//     const { id } = req.params;
-//     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { new: true });
-//     res.render("campgrounds/show", { campground });
-//   })
-// );
-
-// app.delete(
-//   "/campgrounds/:id",
-//   catchAsync(async (req, res) => {
-//     const { id } = req.params;
-//     await Campground.findByIdAndDelete(id);
-//     res.redirect("/campgrounds");
-//   })
-// );
-
-// app.post(
-//   "/campgrounds/:id/reviews",
-//   validateReview,
-//   catchAsync(async (req, res) => {
-//     const campground = await Campground.findById(req.params.id);
-//     const review = new Review(req.body.review);
-//     campground.reviews.push(review);
-//     await review.save();
-//     await campground.save();
-//     res.redirect(`/campgrounds/${campground._id}`);
-//   })
-// );
-
-// app.delete(
-//   "/campgrounds/:id/reviews/:reviewId",
-//   catchAsync(async (req, res) => {
-//     const { id, reviewId } = req.params;
-//     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-//     await Review.findByIdAndDelete(reviewId);
-//     res.redirect(`/campgrounds/${id}`);
-//   })
-// );
 
 // ***Initial db save***
 // app.get("/makecampground", async (req, res) => {
