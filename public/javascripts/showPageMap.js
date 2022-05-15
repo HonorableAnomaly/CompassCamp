0e8a7957e693d3646e3de094c150d9b2c837df9d
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-05-15 11:20:46 -0700

    Map marker Popup
    public/javascripts/showPageMap.js
    +Changed map style from 'streets' to 'outdoors'
    +.setPopup with offset of 25 and .setHTML set to an h3 with 'campground.title' and a p with 'campground.location'

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,9 +1,12 @@
 mapboxgl.accessToken = mapboxToken;
 const map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/streets-v11",
+  style: "mapbox://styles/mapbox/outdoors-v11",
   center: campground.geometry.coordinates,
   zoom: 8
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`))
+  .addTo(map);
