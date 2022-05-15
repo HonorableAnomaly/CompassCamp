288e893b1d1107bcabcaaa5dcc9c112ea94f4e7d
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-05-15 11:56:26 -0700

    PC/Tablet specific DB solution
    +Separate author lines for build device

--- seeds/index.js
+++ seeds/index.js
@@ -24,6 +24,9 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      // PC
+      // author: "6280630d6ad55982d33cf6dd",
+      // Tablet
       author: "6280630d6ad55982d33cf6dd",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
